53fb1a21523614c2ba2cc3872c9cf4f9af4226b9
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-06-17 23:19:02 +0530

    Add specialized domain term IDF weights mapping

---
+++ utils/embeddings/domainIdf.js
@@ -0,0 +1,26 @@
+const DOMAIN_IDF = {
+  'data': 0.6,
+  'database': 0.65,
+  'system': 0.6,
+  'model': 0.7,
+  'query': 0.75,
+  'document': 0.5,
+  'file': 0.55,
+  'information': 0.6,
+  'process': 0.7,
+  'application': 0.75,
+  'user': 0.65,
+  'result': 0.7,
+  'schema': 1.4,
+  'transaction': 1.45,
+  'index': 1.35,
+  'concurrency': 1.5,
+  'retrieval': 1.3,
+  'embedding': 1.4,
+  'vector': 1.35,
+  'compliance': 1.45,
+  'audit': 1.4,
+  'risk': 1.4
+};
+
+module.exports = { DOMAIN_IDF };
