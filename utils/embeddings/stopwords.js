48d3e3d30362268f47a4faf9d86515b6c9792307
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-06-17 23:18:28 +0530

    Add stopword vocabulary filtering list for embedder

---
+++ utils/embeddings/stopwords.js
@@ -0,0 +1,19 @@
+const STOPWORDS = new Set([
+  'the', 'a', 'an', 'and', 'or', 'but', 'if', 'then', 'else', 'when',
+  'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+  'through', 'during', 'before', 'after', 'above', 'below', 'to',
+  'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
+  'again', 'further', 'then', 'once', 'here', 'there', 'all', 'any',
+  'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such',
+  'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
+  'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now',
+  'is', 'am', 'are', 'was', 'were', 'be', 'been', 'being', 'have',
+  'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'i', 'me',
+  'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your',
+  'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself',
+  'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they',
+  'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who',
+  'whom', 'this', 'that', 'these', 'those'
+]);
+
+module.exports = { STOPWORDS };
